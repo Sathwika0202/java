6ad372c927960bd309691a5362ac31c2f18ee4f2
Sathwika0202 <139248088+Sathwika0202@users.noreply.github.com>
2024-08-01 11:49:54 +0530

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -11,5 +11,66 @@ public class Main{
 
 /*-------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------*/
-// 
+// basic methods
+
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        //concateation
+        String firstName="Tony";
+        String lastName = "strak";
+        String fullName = firstName+"@"+lastName;
+        System.out.println(fullName.length());
+        System.out.println(fullName);
+        //charAt
+        for(int i=0;i<fullName.length();i++){
+            System.out.println(fullName.charAt(i));
+        }
+    }
+}
+
+
+
+/*-------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------*/
+// using compare to function
+
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        //compare Strings
+        String s1="tony";
+        String s2="tony";
+        
+        //1 s1 > s2 : +ve value
+        //2 s1==s2 : 0
+        //3 s1<s2 : -ve value
+        
+        //hello wello
+        
+        if(s1.compareTo(s2)==0){
+            System.out.println("strings are equal");
+        }else{
+            System.out.println("strings are not equal");
+        }
+        
+    }
+}
+
+/*-------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------*/
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
   
