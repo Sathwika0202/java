ac79ab454d302234027cdef8338a2128fdc474d3
Sathwika0202 <139248088+Sathwika0202@users.noreply.github.com>
2024-08-01 11:28:43 +0530

    Create Strings.java

---
+++ Strings.java
@@ -0,0 +1,15 @@
+// basic code for defining strings
+
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        String name="Tony";
+        String fullName = "Tony starl";
+        System.out.println(name+fullName);
+    }
+}
+
+/*-------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------*/
+// 
+  
