e376e3436556bee71a5433dc16203bbf2404bdd2
Sathwika0202 <139248088+Sathwika0202@users.noreply.github.com>
2024-08-01 12:23:35 +0530

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -133,7 +133,26 @@ public class Main{
 
 /*-------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------*/
-// reverse a string
+// reverse a string using stringBuilder
+
+public class Main{
+    public static void main(String[]args){
+        StringBuilder sb = new StringBuilder("hello");
+        System.out.println(sb);
+        
+        for(int i=0 ; i<sb.length()/2 ; i++){
+            int front = i;
+            int back = sb.length()-1-i;
+            
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+            
+            sb.setCharAt(front,backChar);
+            sb.setCharAt(back,frontChar);
+        }
+        System.out.println(sb);
+    }
+}
 
 
 
