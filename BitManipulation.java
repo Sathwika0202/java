c12a3d140a9e1db705b354add11431dbcfa4369a
Sathwika0202 <139248088+Sathwika0202@users.noreply.github.com>
2024-08-06 21:09:46 +0530

    Update BitManipulation.java

--- BitManipulation.java
+++ BitManipulation.java
@@ -54,7 +54,30 @@ class Main{
 
 // Update bit
 
-
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class Main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int oper = sc.nextInt();
+        int n = 5;
+        int pos = 1;
+        
+        int bitMask = 1<<pos;
+        if(oper == 1){
+            int newNum = bitMask | n;
+            System.out.println(newNum);
+        }else{
+            int newBitMask = ~(bitMask);
+            int newNum = newBitMask & n;
+            System.out.println(newNum);
+            
+        }
+        
+        
+    }
+}
 
 
 
