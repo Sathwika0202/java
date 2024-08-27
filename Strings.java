2d3acd42b0bf7b7ed59ae4746ace3e830b0a5b9b
Sathwika0202 <139248088+Sathwika0202@users.noreply.github.com>
2024-08-27 13:08:56 +0530

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -156,9 +156,35 @@ public class Main{
 
 
 
+/*-------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------*/
+// anagram checker
 
-
-
+import java.util.Arrays;
+public class Main{
+    public static void main(String[]args){
+        String s1 = "silent";
+        String s2 = "listen";
+        
+        if(anagram(s1,s2)){
+            System.out.println("yes");
+        }else{
+            System.out.println("no");
+        }
+    }
+    public static boolean anagram(String s1, String s2){
+        if(s1.length()!=s2.length()){
+            return false;
+        }
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        
+        return Arrays.equals(c1,c2);
+    }
+}
 
 
 
